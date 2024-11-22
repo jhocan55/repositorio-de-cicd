240c0302af990718925c75d71938c8adf04fb280
jhocan55 <jhon.castaneda.angulo@gmail.com>
2024-11-22 12:24:57 +0100

    second commit first try with terraform configuration in aws

---
+++ states.tf
@@ -0,0 +1,12 @@
+terraform{
+    backend "s3" {
+        bucket = "luna-terraform-state"
+        encrypt = true
+        key = "terraform1.tfstate"
+        region = "eu-west-3"
+    }
+}
+
+provider "aws" {
+  region  = "eu-west-3"
+}
