3fbc72757c347466f1c729b070a42201e1e13b98
jhocan55 <jhon.castaneda.angulo@gmail.com>
2024-11-22 12:14:53 +0100

    delete files previous

--- 1.codestarconnetions.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_codestarconnections_connection" "example" {
-  name          = "bitbucket_connection"
-  provider_type = "Bitbucket"
-}
-
--- 2-frontend/var2frontend.tf
+++
@@ -1,8 +0,0 @@
-variable "S3FrontEnd"{
-    type = string
-        default = null
-}
-variable "name_frontend"{
-    type = string
-        default = null
-}
